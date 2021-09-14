bbfd7757ab1b761ed639f0f7a742b146a3479348
parthdhake <parthdhake@gmail.com>
2021-09-14 15:00:52 +0530

    Requirements

--- home/models.py
+++ home/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Board(models.Model):
+    board_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Card(models.Model):
+    card_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
